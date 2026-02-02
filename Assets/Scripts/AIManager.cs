66399010c036f8dd8e0e5a39b362b719567bf942
mathewgut <mathewgutoskie@gmail.com>
2026-02-02 16:24:05 -0500

    finish environment, add explosion particles, add more sound effects

--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -3,27 +3,78 @@ using UnityEngine;
 
 public class AIManager : MonoBehaviour
 {
-
     public GameObject Agent;
     public GameObject target;
 
     public List<GameObject> spawnPoints = new List<GameObject> { };
 
+    public AudioSource splatSource;
+    public AudioSource specialSource;
+    public ParticleSystem bloodParticles;
+
     Transform chosenSpawn;
+    GameObject agentInstance;
+
+    float bloodTimeStart;
+    float destroyTimeStart;
+
+
+    int tick;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         chosenSpawn = spawnPoints[Random.Range(0, spawnPoints.Count)].transform;
 
-        GameObject agentInstance = Instantiate(Agent,chosenSpawn);
+        agentInstance = Instantiate(Agent, chosenSpawn.position, Quaternion.identity);
         agentInstance.GetComponent<CustomerController>().target = target;
 
+        bloodParticles.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tick += 1;
+
+        if(tick % 4 == 0)
+        {
+            if(agentInstance.TryGetComponent<CustomerController>(out CustomerController customer))
+            {
+                if (customer.orderComplete)
+                {
+                    if(destroyTimeStart == -1)
+                    {
+                        destroyTimeStart = Time.time;
+                    }
+
+                    if (!specialSource.isPlaying)
+                    {
+                        specialSource.Play();
+                    }
+                    
+                    
+                    if (Time.time - destroyTimeStart >= 0.15f)
+                    {
+                        bloodParticles.Play();
+                        
+                        splatSource.Play();
+                        Destroy(agentInstance);
+                        bloodTimeStart = Time.time;
+                        chosenSpawn = spawnPoints[Random.Range(0, spawnPoints.Count)].transform;
+                        agentInstance = Instantiate(Agent, chosenSpawn.position, Quaternion.identity);
+                        agentInstance.GetComponent<CustomerController>().target = target;
+                        destroyTimeStart = -1;
+                    }
+                }
+            }
+            
+            if(Time.time - bloodTimeStart >= 0.3f)
+            {
+                bloodParticles.Stop();
+            }
+
+        }
+
     }
 }
--- Assets/Scripts/CustomerController.cs
+++ Assets/Scripts/CustomerController.cs
@@ -1,16 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
+using System;
 
 public class CustomerController : MonoBehaviour
 {
     public GameObject target;
-    public string acceptedItemTag;
+    public string acceptedItemTag = "coffee_cup";
+
+    public int minCoffeeCount = 1;
+    public int maxCoffeeCount = 4;
+
+    public Canvas orderUI;
+    public TextMeshProUGUI orderQuantity;
+
+    int coffeeCount;
+    public bool orderComplete = false;
+
+    public ParticleSystem bloodParticles;
+
 
     NavMeshAgent agent;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        
+        coffeeCount = UnityEngine.Random.Range(minCoffeeCount, maxCoffeeCount + 1); // max is exclusive, +1 to include full range
+        UpdateUI();
+        orderUI.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,11 +37,55 @@ public class CustomerController : MonoBehaviour
         {
             agent.destination = target.transform.position;
         }
-    }
 
+        if (agent.transform.position.x == target.transform.position.x && // y position is inheriently different between the empty and ai
+            agent.transform.position.z == target.transform.position.z)
+        {
+            orderUI.gameObject.SetActive(true);
+        }
+
+        orderUI.transform.LookAt(Camera.main.transform);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log("colliding");
+
+        if (collision.gameObject.TryGetComponent<PickupBehaviour>(out PickupBehaviour item))
+        {
+            if (item.id == acceptedItemTag && IsCupFull(item.gameObject))
+            {
+                coffeeCount -= 1;
+                Destroy(collision.gameObject);
+                CheckOrder();
+            }
+        }
     }
+
+    bool IsCupFull(GameObject cup)
+    {
+        if(cup.TryGetComponent<FillableBehaviour>(out FillableBehaviour item))
+        {
+            return item.currentAmount >= item.maxAmount;
+        }
+        return false;
+    }
+
+    // when customer recieves a valid item, check if order is complete or not
+    void CheckOrder()
+    {
+        if(coffeeCount <= 0)
+        {
+            orderComplete = true;
+        }
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        orderQuantity.text = "x" + coffeeCount.ToString();  // e.g wants coffee x3
+    }
+
 }
+
+
